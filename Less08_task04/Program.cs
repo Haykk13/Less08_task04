f91c5c70c44550ebd85264b9a644681b23f52d8e
Hayk13 <hayk.hayr3@gmail.com>
2020-12-19 15:22:27 +0400

    Add project files.

---
+++ Less08_task04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Less08_task04
+{
+    enum MyEnum : ushort
+    {
+        zero,
+        one,
+        two
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort u1 = 1;
+            ushort u2 = 2;
+            ushort u3 = 3;
+
+            Console.WriteLine(isEnumValueDefined(u1));
+            Console.WriteLine(isEnumValueDefined(u2));
+            Console.WriteLine(isEnumValueDefined(u3));
+
+            Console.ReadKey();
+        }
+
+        static bool isEnumValueDefined(ushort u)
+        {
+            bool flag = Enum.IsDefined(typeof(MyEnum), u);
+            return flag;
+        }
+    }
+}
